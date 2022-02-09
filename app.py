5a997bc82d6760986ccc759e4546414a3091d2a4
Arib Nawal <65786174+arib168@users.noreply.github.com>
2022-02-09 13:58:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ ip = st.text_input('Enter your message:')
 # predict if the entered message is spam or ham 
 op = model.predict([ip])
 if st.button('PREDICT'):    #create a button called predict, if we click on this button, display if the entered message was spam or ham 
-  st.title(op[0])
+  st.title(op[0]) 
